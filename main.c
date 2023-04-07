650be4750a3869578f30f1d523921a571f724519
5210505034 <127401273+5210505034@users.noreply.github.com>
2023-04-07 22:49:29 +0300

    Update main.c

--- main.c
+++ main.c
@@ -2,9 +2,11 @@
 #include <stdlib.h>
 #include <stdlib.h>
 #include <limits.h>
+//gerekli kütüphaneler include edildi.
 int main() {
     int boyut;
     int eleman;
+    //girilecek dizinin boyut bilgisi alındı.
     int *dizi;
     int hedef;
     printf("karsilastirilacak sayiyi giriniz.\n");
@@ -13,17 +15,18 @@ int main() {
     scanf("%d", &boyut);
     printf("%d", boyut);
     dizi = (int *) malloc(boyut * sizeof(int));
+    //pointer ile dizinin boyutu kullanıcının girdiği boyut değişkeni ile değiştirildi.
     
     for (int i = 0; i < boyut; i++){
         printf("dizinin %d. indexini giriniz.\n", i);
         scanf("%d", &eleman);
         dizi[i] = eleman;
     }
-    
+    //bu döngüde kullanıcıdan dizinin elemanları alındı.
     for (int j = 0; j < boyut; j ++){
         printf("dizinin %d. elamani = %d\n", j,dizi[j]);
     }
-    
+    //alınan elemanlar ekrana yazdırıldı.
     printf("karsilastirilacak sayi %d\n", hedef);
     en_yakin_toplam(dizi, boyut, hedef);
     return 0;
@@ -33,21 +36,23 @@ void en_yakin_toplam(int dizi[], int n, int hedef) {
     int en_i, en_j, en_k;
     int en_uzak = INT_MAX;
     int i, j, k;
- 
+    //3 tane değer arandığı için ilk döngü dizinin boyutu -2 kadar 2. döngü dizinin boyutu -1 kadar ve 3. döngü dizinin boyutu kadar döndürülmüştür.
     for (i = 0; i < n - 2; i++) {
         for (j = i + 1; j < n - 1; j++) {
             for (k = j + 1; k < n; k++) {
                 int toplam = dizi[i] + dizi[j] + dizi[k];
                 int fark = abs(hedef - toplam); 
- 
-                if (fark < en_uzak) {
+                 //hedef ve toplamın farkını alıp mutlak değer olarak fark değişkenine atandı. 
+                if (fark < en_uzak)  { //fark değişkeni en uzak değişkeninden küçük ise 
                     en_uzak = fark;
                     en_i = i;
                     en_j = j;
                     en_k = k;
+                    //en uzak değişkeni fark değişkenine eşitlendi döngüdeki mevcut i,j,k değerleri kullanılacak değişkenlere atandı.
                 }
             }
         }
     }
     printf("En yakın toplam: %d + %d + %d = %d\n", dizi[en_i], dizi[en_j], dizi[en_k], dizi[en_i]+dizi[en_j]+dizi[en_k]);
+    //kullanıcıya bulunan değerler gösterilmiştir.
 }
